2d73b952c260f000dd4ec94cfa179aaa98392804
Kamalei Simon <simonk21@up.edu>
2020-02-09 01:16:26 -0800

    Editing account.js functionality w/ comments

--- public/javascripts/account.js
+++ public/javascripts/account.js
@@ -1,29 +1,50 @@
 function editMode(){
+    // div variables 
     var btn = document.getElementById('edit-btn');
-    if(btn.innerHTML == "Save Profile"){
+    var bioDiv = document.getElementById('bio-div');
+    var nameDiv = document.getElementById('name-div');
+    if(btn.innerHTML == "Save Profile"){ // button - transition to saving profile 
 	var fTextArea = document.getElementById('first-text');
 	var lTextArea = document.getElementById('last-text');
+	var bTextArea = document.getElementById('bio-text');
 	var first = fTextArea.value;
 	var last = lTextArea.value;
+	var bio = bTextArea.value;
+	// changing name, removing textarea
 	document.getElementById('name').innerHTML = first + " " + last;
-	document.getElementById('name-div').removeChild(fTextArea);
-	document.getElementById('name-div').removeChild(lTextArea);
-	btn.innerHTML = "Edit Profile";
+	nameDiv.removeChild(fTextArea);
+	nameDiv.removeChild(lTextArea);
+
+	// changing bio, removing textarea 
+	var para = document.createElement("p");
+	para.innerHTML = bio;
+	para.id = "bio";
+	bioDiv.removeChild(bTextArea);
+	bioDiv.appendChild(para);
+	btn.innerHTML = "Edit Profile"; // changing button
     }
-    else{
+    else{ // button - transition to editing profile
     var name = document.getElementById('name').innerHTML;
     var matches = name.match(/([A-Z][a-z]+) ([A-Z][a-z]+)/);
-	if(matches){
+	if(matches){ // creating textarea for first and last name
 	var first = document.createElement("textarea");
 	first.innerHTML = matches[1];
 	first.id = "first-text";
 	var last = document.createElement("textarea");
 	last.innerHTML = matches[2];
 	last.id = "last-text";
-	document.getElementById('name-div').appendChild(first);
-	document.getElementById('name-div').appendChild(last);
+	nameDiv.appendChild(first);
+	nameDiv.appendChild(last);
 	}
-	btn.innerHTML = "Save Profile";
+
+	// creating textarea for bio
+	var bio = document.getElementById('bio');
+	var bioTextArea = document.createElement("textarea");
+	bioTextArea.innerHTML = bio.innerHTML;
+	bioTextArea.id = 'bio-text';
+	bioDiv.appendChild(bioTextArea);
+	bioDiv.removeChild(bio);
+	btn.innerHTML = "Save Profile"; // changing button
     }
 }
 

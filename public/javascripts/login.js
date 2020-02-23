aa3433dd511c674d46c367a7706ad0de475ef476
Kamalei Simon <simonk21@up.edu>
2020-02-23 00:21:38 -0800

    linking login to account page

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,9 +1,13 @@
 function login(){
     var username = document.getElementById('loginUsername').value;
     var password = document.getElementById('loginPassword').value;
-    $.post("localhost:3000/login?username=" +username +'&password=' +password, function(data){
+    $.post("/login?username=" +username +'&password=' +password, function(data){
         if(data[0] != null){
             document.getElementById('id01').style.display='none';
+            var div_btn = document.getElementById('loginbtns');
+            div_btn.innerHTML = "<button style='width:100px' id='acct'>Account</button>"
+            var btn = document.getElementById('acct');
+            btn.setAttribute("onclick","window.location.href = 'accountpage.html';");
         } else{
             document.getElementById('invalidLogin').innerHTML = "Invalid Username or Password";
         }

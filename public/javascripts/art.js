ac3870f7a53b5d662509e9f5598990d562a5a04d
Kamalei Simon <simonk21@up.edu>
2020-02-23 17:54:36 -0800

    added scrollable

--- public/javascripts/art.js
+++ public/javascripts/art.js
@@ -1,9 +1,16 @@
 function init() {
     $.post('/retrieveart', function (art) { // displays title of art TODO: add more info and possible links
-        var html = "<table><tr>";
+        var html = "<table width='600'><tr>";
         for (var i = 1; i < art.length; i++) {
-            html += "<td>" + art[i].TITLE + "</td>";
+            if(art[i].TITLE != ''){
+                var title = "<h2>" + art[i].TITLE + "</h2>";
+                var location = "<p>" + art[i].LOCATIONNAME + "</p>";
+                var creatorInfo = "<p>" + art[i].CREATOR + " " + art[i].DATE + "</p>";
+                html += "<td>"
+                html += title + location + creatorInfo;
+                html += "</td>";
                 html += "</tr><tr>";
+            }
         }
         html += "</tr></table>";
         document.getElementById("artdisplay").innerHTML = html;

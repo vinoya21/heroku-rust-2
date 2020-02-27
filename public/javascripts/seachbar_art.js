cf52035c83ceb3f4326b5cacf9465c1269e5fb3a
Mikey <mikerichantk@gmail.com>
2020-02-26 16:59:46 -0800

    Basic Searchbar Setups
    Fixed bug with artpage searchbar
    reformatted search bars
    fixed bug with small text in searchbar
    added a list for future testing of searchbar functionality on outdoor page

--- public/javascripts/seachbar_art.js
+++
@@ -1,8 +0,0 @@
-// searchbar_art.js
-// Rust Team Project
-
-function searchArt() {
-    var searchText = document.getElementById("searchbar_input_art");
-    var searchInputArt = searchText.value;
-    alert(searchInputArt);
-}

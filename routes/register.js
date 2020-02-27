72a2f66ea63004a6531fa4d53254fe3e506224d6
Kamalei Simon <simonk21@up.edu>
2020-02-26 17:55:50 -0800

    updated signup functionality

--- routes/register.js
+++ routes/register.js
@@ -10,41 +10,41 @@ router.post('/', function (req, res) {
     var validRegistration = true;
     var usernameFound = false;
     var emailFound = false;
-    dbms.dbquery("SELECT * FROM USERS WHERE USERNAME='" +req.query.username +"'",
+    dbms.dbquery("SELECT * FROM USERS WHERE USERNAME='" + req.query.username + "'",
         function (error, result) {
             if (error) {
                 console.log("ERROR: " + error);
                 return;
             }
-            if(result != null){
+            if (result.length != 0) {
                 usernameFound = true;
                 validRegistration = false;
             }
+            dbms.dbquery("SELECT * FROM USERS WHERE EMAIL='" + req.query.email + "'",
+                function (error, result) {
+                    if (error) {
+                        console.log("ERROR: " + error);
+                        return;
+                    }
+                    if (result.length != 0) {
+                        emailFound = true;
+                        validRegistration = false;
+                    }
+                    //if no matching username or email, adds info to users table, and returns success
+                    if (validRegistration == true) {
+                        dbms.dbquery("INSERT INTO USERS (USERNAME, FIRSTNAME, LASTNAME, EMAIL, PASSWORD) VALUES ('" + req.query.username + "','" + req.query.firstName + "','" + req.query.lastName + "','" + req.query.email + "','" + req.query.password + "')",
+                            function (error, result) {
+                                if (error) {
+                                    console.log("ERROR: " + error);
+                                    return;
+                                }
+                                res.send("success");
+                            });
+                    } else { //if matching found, no user added and error message returned
+                        res.send("usernameFound=" + usernameFound + "emailFound=" + emailFound);
+                    }
+                });
         });
-    dbms.dbquery("SELECT * FROM USERS WHERE EMAIL='" +req.query.email +"'",
-        function (error, result) {
-            if (error) {
-                console.log("ERROR: " + error);
-                return;
-            }
-            if(result != null){
-                emailFound = true;
-                validRegistration = false;
-            }
-        });
-    //if no matching username or email, adds info to users table, and returns success
-    if(validRegistration == true){
-        dbms.dbquery("INSERT INTO USERS (USERNAME, FIRSTNAME, LASTNAME, EMAIL, PASSWORD) VALUES ('" +req.query.username +"','" +req.query.firstName +"','" +req.query.lastname +"','" +req.query.email +"','" +req.query.password +"')",
-        function (error, result) {
-            if (error) {
-                console.log("ERROR: " + error);
-                return;
-            }
-            res.send("success");
-        });
-    } else{ //if matching found, no user added and error message returned
-        res.send("usernameFound=" +usernameFound +"emailFound=" +emailFound);
-    }
 });
 
 module.exports = router;

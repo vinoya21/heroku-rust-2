700ab0e099c95847649a787179a40d9d1e28d197
Kamalei Simon <simonk21@up.edu>
2020-03-14 22:41:19 -0700

    added favorites to account page
    -- there's no functionality besides displaying favorites
    -- would like user to be able to remove favorites and for it to be organized

--- routes/retrieveFavorite.js
+++ routes/retrieveFavorite.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var db = require('./dbms');
 router.post('/', function (req, res, next) {
     var user = req.query.user;
-    var title = req.query.title;
     db.dbquery("SELECT * FROM USERS WHERE USERNAME = '" + user + "'", function (err, result) {
         res.json(result);
     });

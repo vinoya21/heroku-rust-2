bf784f64572c64be24213fa4ded33c8e5a7efe17
Mikey <mikerichantk@gmail.com>
2020-03-18 18:22:32 -0700

    added javascript files
    Added javascript files for the other pages
    started the code for the functionality of the searchbars once their databases are inplimented

---
+++ public/javascripts/recreation.js
@@ -0,0 +1,20 @@
+/* recreation page javascript */
+function search_rec() {
+    let input = document.getElementById('searchbar_input_rec').value
+    input = input.toLowerCase();
+    /* make this functional when there is a table in event page
+        go to table.css to enable functionality
+        still need to add onkeyup when all are pushed to master
+    let x = document.getElementsByClassName('artclass');
+    */
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+        }
+        else {
+            x[i].style.display = "table-cell";
+        }
+    }
+
+
+}
